eb4c03a4ca9b7c1b78a40a2f83840cae48af8b22
mdsajalali <mdsajalali2020@gmail.com>
2024-01-04 22:10:06 +0600

    header section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Header from "./pages/Home/Header"
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className="bg-[#FAFFF1] min-h-screen">
+      <div className="max-w-[1230px] mx-auto">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App
