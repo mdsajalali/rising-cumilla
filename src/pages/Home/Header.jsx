6b8cdb66f97edbf7bf62fa7de63184eeea3cae28
mdsajalali <mdsajalali2020@gmail.com>
2024-01-04 22:38:15 +0600

    header text added

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -1,84 +1,102 @@
+import { FaFacebook, FaPinterest, FaTwitter, FaYoutube } from "react-icons/fa";
 import logo from "../../assets/images/logo.webp";
 const Header = () => {
   return (
-    <div className="navbar">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <>
+      <div className="navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li className="list-none">
+                <a href="#">প্রচ্ছদ</a>
+              </li>
+              <li className="list-none">
+                <a href="#">জাতীয়</a>
+              </li>
+              <li className="list-none">
+                <a href="#">সারা দেশ</a>
+              </li>
+              <li className="list-none">
+                <a href="#">কুমিল্লা</a>
+              </li>
+              <li className="list-none">
+                <a href="#">বিশ্ব</a>
+              </li>
+              <li className="list-none">
+                <a href="#">অর্থ ও বাণিজ্য</a>
+              </li>
+              <li className="list-none">
+                <a href="#">অর্থ ও বাণিজ্য</a>
+              </li>
+              <li className="list-none">
+                <a href="#">স্বাস্থ্য</a>
+              </li>
+              <li className="list-none">
+                <a href="#">খেলা</a>
+              </li>
+              <li className="list-none">
+                <a href="#">বিনোদন</a>
+              </li>
+              <li className="list-none">
+                <a href="#">শিক্ষা</a>
+              </li>
+              <li className="list-none">
+                <a href="#">লাইফস্টাইল</a>
+              </li>
+              <li className="list-none">
+                <a href="#">জবস</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Homepage</a>
-            </li>
-            <li>
-              <a>Portfolio</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
+        </div>
+        <div className="navbar-center">
+          <a className="btn btn-ghost text-xl">
+            <img className="w-56" src={logo} alt="logo" />
+          </a>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <FaFacebook size={25} />
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <FaTwitter size={25} />
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <FaYoutube size={25} />
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <FaPinterest size={25} />
+          </button>
         </div>
       </div>
-      <div className="navbar-center">
-        <a className="btn btn-ghost text-xl">
-          <img className="w-56" src={logo} alt="logo" />
-        </a>
+      <div className="flex items-center justify-between ml-5">
+        <p>জানুয়ারি ৪, ২০২৪ ৩:৩৯ পিএম</p> 
+        <p>Contribute News কনভার্টার</p>
       </div>
-      <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-          </div>
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
