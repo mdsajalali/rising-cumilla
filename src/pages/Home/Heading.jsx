2c69d72aca54b4af0f5b847b139be95c129b633a
mdsajalali <mdsajalali2020@gmail.com>
2024-01-06 18:48:28 +0600

    Heading resposive added

--- src/pages/Home/Heading.jsx
+++ src/pages/Home/Heading.jsx
@@ -1,9 +1,9 @@
 import heading from "../../assets/images/heading.webp";
 const Heading = () => {
   return (
-    <div className="max-w-[1230px] mx-auto border mb-5">
+    <div className="max-w-[1230px] mx-auto border mb-5 mt-4">
       <img
-        className="w-9/12 mx-auto h-[80px] object-cover"
+        className=" w-full mx-auto md:px-10 xl:px-20 px-5 object-contain    "
         src={heading}
         alt="Heading"
       />
